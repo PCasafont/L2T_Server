112d404875d92c06e0745374df7fab894f5f9216
Pere Casafont <pere@pcasafont.net>
2016-11-01 16:10:35 +0100

    Augments fix

--- sql/server/item_attributes.sql
+++ sql/server/item_attributes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `item_attributes` (
   `itemId` int(11) NOT NULL DEFAULT '0',
-  `augAttributes` int(11) NOT NULL DEFAULT '-1',
+  `augAttributes` bigint(21) NOT NULL DEFAULT '-1',
   PRIMARY KEY (`itemId`),
   FOREIGN KEY (`itemId`) REFERENCES `items`(`object_id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
